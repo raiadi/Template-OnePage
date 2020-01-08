b55bc93a74a08b38e4eaca25f3be5c1f610edebc
Adarsh Rai <adarsh@adarshs-mbp.home>
2020-01-08 00:10:01 +0000

    server working, home page added and routed

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 
-
 //Start App
 const app = express();
 
-
 //Server port
 let portNum = 3004;
-const port = process.env.PORT || portNum;
+let port = process.env.PORT || portNum;
+
+app.get('/', function(req,res){
+    res.sendFile("/Users/adarsh/Desktop/GitHub Projects/Template-OnePage/modules/app/client/view/home.html");
+});
+
+app.get('*', function(req, res){
+    res.send('404 Error');
+});
 
 //Start Server
-let server = (port, function(req,res){
+let server = app.listen(port, function(req,res){
     console.log('Server listening to port', portNum);
 });
